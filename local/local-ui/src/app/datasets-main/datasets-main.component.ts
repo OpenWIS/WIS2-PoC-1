83914c7ced28d8b180505fe1f011ac1a397c13af
dimitris.papadeas <dimitris.papadeas@eurodyn.com>
2017-11-22 17:29:16 +0200

    adding new component

---
+++ local/local-ui/src/app/datasets-main/datasets-main.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-datasets-main',
+  templateUrl: './datasets-main.component.html',
+  styleUrls: ['./datasets-main.component.css']
+})
+export class DatasetsMainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
