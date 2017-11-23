3a7aa9408b9bd57e97f65e8ca3243e14ac250f4a
George Triantafyllidis <giorgos.triantafyllidis@eurodyn.com>
2017-11-23 14:24:02 +0200

    Button-links

--- local/local-ui/src/app/datasets-admin/datasets-admin.component.ts
+++ local/local-ui/src/app/datasets-admin/datasets-admin.component.ts
@@ -1,36 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-import {MatTableDataSource} from '@angular/material';
-
+import { Component, OnInit } from "@angular/core";
+import { MatTableDataSource } from "@angular/material";
+import { RouterModule, Routes, Router } from "@angular/router";
 
 @Component({
-  selector: 'app-datasets-admin',
-  templateUrl: './datasets-admin.component.html',
-  styleUrls: ['./datasets-admin.component.css']
+  selector: "app-datasets-admin",
+  templateUrl: "./datasets-admin.component.html",
+  styleUrls: ["./datasets-admin.component.css"]
 })
 export class DatasetsAdminComponent implements OnInit {
-  displayedColumns = ['name', 'description', 'url'];
+  displayedColumns = ["name", "description", "url"];
   dataSource = new MatTableDataSource<Element>(datasetList);
 
-  constructor() { }
+  constructor(private router: Router) {}
 
+  ngOnInit() {}
 
-  ngOnInit() {
+  navigateToEditDataset(){
+    //this.router.navigateByUrl('/datasets');
   }
-  
-  
 
+  navigateToViewDataset(){
+    //this.router.navigateByUrl('/datasets');
+  }
 }
 export interface Element {
   name: string;
   description: string;
-  url:string;
+  url: string;
 }
 
-
-  const datasetList: Element[] = [
-  {name: 'Athens', description:"Temperature/Rainfall in Athens"},
-  {name: 'Thesaloniki', description:"Temperature/Rainfall in Thesaloniki"},
-  {name: 'Iraklion', description:"Wind speed/direction in Iraklion"},
-
+const datasetList: Element[] = [
+  { name: "Athens", description: "Temperature/Rainfall in Athens" },
+  { name: "Thesaloniki", description: "Temperature/Rainfall in Thesaloniki" },
+  { name: "Iraklion", description: "Wind speed/direction in Iraklion" }
 ];
-
