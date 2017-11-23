ee839dc6cfe0576515b9fe77e36e447165e84071
dimitris.papadeas <dimitris.papadeas@eurodyn.com>
2017-11-23 13:03:15 +0200

    adding sidenav autohide when on home page

--- local/local-ui/src/app/app.component.ts
+++ local/local-ui/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component} from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { SubmitFormModule } from './submit-form/submit-form.module';
-import { RouterModule, Routes } from '@angular/router'; 
+import { RouterModule, Routes, Router } from '@angular/router';
+import { MatSidenav } from '@angular/material';
 
 
 @Component({
@@ -11,7 +12,35 @@ import { RouterModule, Routes } from '@angular/router';
 })
 
 
-export class AppComponent {
+
+export class AppComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+
+  @ViewChild('sidenav') private sidenav: MatSidenav;
+  
+  currentUrl: string;
+
+  ngOnInit() {
+    let currentUrl = this.router.url;
+  }
   title = 'app';
+  showhideCondrols: boolean = false;
+
+
+  hideShowControls(): boolean {
+    let currentUrl = this.router.url;
+    
+    
+    if (this.router.url === '/home'){
+      this.showhideCondrols = false;
+      this.sidenav.close();
+    }else{
+      this.showhideCondrols = true;
+    }
+    return this.showhideCondrols;
+  }
+
 }
 
