ee1d44915050f4b26f57cc50141cf625afda400a
dimitris.papadeas <dimitris.papadeas@eurodyn.com>
2017-11-30 17:16:35 +0200

    awisc sceleton

--- awisc/awisc-ui/src/app/app.component.ts
+++ awisc/awisc-ui/src/app/app.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { RouterModule, Routes, Router } from '@angular/router';
+import { MatSidenav } from '@angular/material';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
+
 })
-export class AppComponent {
+
+
+
+export class AppComponent implements OnInit {
+
+  @ViewChild('sidenav') private sidenav: MatSidenav;
+
+  currentUrl: string;
+  static selectedMenuItem: any;
+  static menuOpen: boolean = true;
+
+  constructor(private router: Router) { 
+    
+  }
+
+  onOpenedChange(){
+    AppComponent.menuOpen = this.sidenav.opened;
+  }
+
+
+  ngOnInit() {
+    let currentUrl = this.router.url;
+  }
   title = 'app';
+  showhideCondrols: boolean = false;
+
+
+  hideShowControls(): boolean {
+    let currentUrl = this.router.url;
+    
+    
+    if (this.router.url === '/home'){
+      this.showhideCondrols = false;
+      this.sidenav.close();
+    }else{
+      this.showhideCondrols = true;
+    }
+    return this.showhideCondrols;
+  }
+
+  
+  clicked(object) {
+    AppComponent.selectedMenuItem = object;
+  }
+
+  get selectedMenuItem() {
+    return AppComponent.selectedMenuItem;
+  }
+
+  navigateToMenu(){
+    this.router.navigateByUrl('/awiscSearch');
+  }
+
+  ngAfterViewInit(){
+  }
 }
+
--- awisc/awisc-ui/src/app/app.module.ts
+++ awisc/awisc-ui/src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { routing } from './app.routes';
 import { AppComponent } from './app.component';
+import {SettingsComponent} from './settings/settings.component';
+import { AwiscMainComponent } from './awisc-main/awisc-main.component';
+import { AwiscSearchComponent } from './awisc-search/awisc-search.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SettingsComponent,
+    AwiscMainComponent,
+    AwiscSearchComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    routing   
   ],
+  
+  
   providers: [],
-  bootstrap: [AppComponent]
+   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+
+export class AppModule {}
+
+
+
+
