cb462e3f91c8c1c43c58e8fb28fb887ecbfc2207
dimitris.papadeas <dimitris.papadeas@eurodyn.com>
2017-12-01 10:54:55 +0200

    update search component

--- awisc/awisc-ui/src/app/awisc-search/awisc-search.component.ts
+++ awisc/awisc-ui/src/app/awisc-search/awisc-search.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AppComponent } from '../app.component';
 
 @Component({
   selector: 'app-awisc-search',
   templateUrl: './awisc-search.component.html',
   styleUrls: ['./awisc-search.component.css']
 })
+
+
 export class AwiscSearchComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) {
+    AppComponent.selectedMenuItem = 'awiscSearch';
+  }
 
   ngOnInit() {
   }
+
+
+
+  ngAfterViewInit() {
+    if (!AppComponent.menuOpen) {
+      document.getElementById('sitenav').click();
+    }
+  }
+
 
 }
