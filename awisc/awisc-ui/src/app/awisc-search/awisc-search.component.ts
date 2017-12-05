f9f502fd54f91e5fed1be4cb831fcd8fa6a91266
dimitris.papadeas <dimitris.papadeas@eurodyn.com>
2017-12-05 15:30:21 +0200

    update search page

--- awisc/awisc-ui/src/app/awisc-search/awisc-search.component.ts
+++ awisc/awisc-ui/src/app/awisc-search/awisc-search.component.ts
@@ -89,7 +89,9 @@ export class AwiscSearchComponent implements OnInit {
 
 
 
+  addSearchCriteria(){
 
+  }
 
   search() {
     // call search service 
@@ -138,35 +140,35 @@ export class AwiscSearchComponent implements OnInit {
 
 // delete:
 
-  windCodes: any = [
-    { label: "Wind direction (from which blowing)", wmocode: "grib2/codeflag/4.2/0-2-0" },
-    { label: "Wind speed", wmocode: "grib2/codeflag/4.2/0-2-1" },
-    { label: "Wind speed", wmocode: "grib2/codeflag/4.2/3-1-19" },
-    { label: "Wind chill factor", wmocode: "grib2/codeflag/4.2/0-0-13" },
-    { label: "Maximum wind level", wmocode: "bufr4/codeflag/0-08-001/4" },
-    { label: "Maximum wind level", wmocode: "bufr4/codeflag/0-08-086/4" },
-  ];
+  // windCodes: any = [
+  //   { label: "Wind direction (from which blowing)", wmocode: "grib2/codeflag/4.2/0-2-0" },
+  //   { label: "Wind speed", wmocode: "grib2/codeflag/4.2/0-2-1" },
+  //   { label: "Wind speed", wmocode: "grib2/codeflag/4.2/3-1-19" },
+  //   { label: "Wind chill factor", wmocode: "grib2/codeflag/4.2/0-0-13" },
+  //   { label: "Maximum wind level", wmocode: "bufr4/codeflag/0-08-001/4" },
+  //   { label: "Maximum wind level", wmocode: "bufr4/codeflag/0-08-086/4" },
+  // ];
 
 
-  rainCodes: any = [
-    { label: "Rain", wmocode: "bufr4/codeflag/0-20-004/16" },
-    { label: "Rain", wmocode: "bufr4/codeflag/0-20-004/6" },
-    { label: "Rain", wmocode: "bufr4/codeflag/0-20-005/6" },
-    { label: "Precipitation of rain", wmocode: "306/4678/RA" },
-    { label: "Precipitation of rain", wmocode: "bufr4/codeflag/0-20-003/280" },
-    { label: "Categorical freezing rain", wmocode: "grib2/codeflag/4.2/0-1-34" },
-    { label: "Reflectivity of rain", wmocode: "grib2/codeflag/4.2/0-15-12" },
-  ];
+  // rainCodes: any = [
+  //   { label: "Rain", wmocode: "bufr4/codeflag/0-20-004/16" },
+  //   { label: "Rain", wmocode: "bufr4/codeflag/0-20-004/6" },
+  //   { label: "Rain", wmocode: "bufr4/codeflag/0-20-005/6" },
+  //   { label: "Precipitation of rain", wmocode: "306/4678/RA" },
+  //   { label: "Precipitation of rain", wmocode: "bufr4/codeflag/0-20-003/280" },
+  //   { label: "Categorical freezing rain", wmocode: "grib2/codeflag/4.2/0-1-34" },
+  //   { label: "Reflectivity of rain", wmocode: "grib2/codeflag/4.2/0-15-12" },
+  // ];
 
-  temperatureCodes: any = [
-    { label: "Apparent temperature", wmocode: "grib2/codeflag/4.2/0-0-21" },
-    { label: "Dewpoint temperature", wmocode: "bufr4/b/12/103" },
-    { label: "Dewpoint temperature", wmocode: "bufr4/b/12/024" },
-    { label: "Dewpoint temperature", wmocode: "grib2/codeflag/4.2/0-0-6" },
-    { label: "Highest daily mean temperature", wmocode: "bufr4/b/12/152" },
-    { label: "Minimum temperature", wmocode: "grib2/codeflag/4.2/0-0-5" },
-    { label: "Minimum temperature", wmocode: "bufr4/codeflag/0-08-050/8" },
-  ];
+  // temperatureCodes: any = [
+  //   { label: "Apparent temperature", wmocode: "grib2/codeflag/4.2/0-0-21" },
+  //   { label: "Dewpoint temperature", wmocode: "bufr4/b/12/103" },
+  //   { label: "Dewpoint temperature", wmocode: "bufr4/b/12/024" },
+  //   { label: "Dewpoint temperature", wmocode: "grib2/codeflag/4.2/0-0-6" },
+  //   { label: "Highest daily mean temperature", wmocode: "bufr4/b/12/152" },
+  //   { label: "Minimum temperature", wmocode: "grib2/codeflag/4.2/0-0-5" },
+  //   { label: "Minimum temperature", wmocode: "bufr4/codeflag/0-08-050/8" },
+  // ];
 
 
 
