69ef80e2a2f6affd751624acc3389637a50ad7b6
George Triantafyllidis <giorgos.triantafyllidis@eurodyn.com>
2018-01-29 17:24:11 +0200

    Fixed datasource reference

--- awisc/awisc-ui/angular/src/app/awisc-main/awisc-main.component.ts
+++ awisc/awisc-ui/angular/src/app/awisc-main/awisc-main.component.ts
@@ -26,40 +26,7 @@ export class AwiscMainComponent implements OnInit {
     this.searchForm = new FormGroup({
       datasetSearch: new FormControl("")
     });
-
-    // This is just for demo! All REST calls should be routed via a Service and the URL prefix
-    // of the back-end be a Constant.
-    // Normal call.
-    //this.http.get("http://localhost:8181/cxf/api/hello-world").subscribe(
-	this.http.get("http://10.100.1.95:8181/cxf/api/hello-world").subscribe(
-        onNext => {
-          console.log("OK1: ", onNext);
-        },
-        onError => {
-          console.error("Error1: ", onError);
-        }
-    )
-
-    // Error call.
-    // this.http.get("http://localhost:8181/cxf/api/hello-world-error").subscribe(
-    //     onNext => {
-    //       console.log("OK2: ", onNext);
-    //     },
-    //     onError => {
-    //       console.error("Error2: ", onError);
-    //     }
-    // )
-
-    // Demo for QueryDSL + MapStruct.
-    this.http.get("http://localhost:8181/cxf/api/hello-world-qdsl").subscribe(
-        onNext => {
-          console.log("OK3: ", onNext);
-        },
-        onError => {
-          console.error("Error3: ", onError);
-        }
-    )
-
+    
   }
 
   ngAfterViewInit(){
