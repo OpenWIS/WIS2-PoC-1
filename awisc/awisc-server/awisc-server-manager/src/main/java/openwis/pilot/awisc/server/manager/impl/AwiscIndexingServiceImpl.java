38da4ec7a4683937c6658993305989b0e6e85d06
George Triantafyllidis <giorgos.triantafyllidis@eurodyn.com>
2018-03-12 13:24:41 +0200

    Removing initial fetch

--- awisc/awisc-server/awisc-server-manager/src/main/java/openwis/pilot/awisc/server/manager/impl/AwiscIndexingServiceImpl.java
+++ awisc/awisc-server/awisc-server-manager/src/main/java/openwis/pilot/awisc/server/manager/impl/AwiscIndexingServiceImpl.java
@@ -173,7 +173,7 @@ public class AwiscIndexingServiceImpl implements AwiscIndexingService, Serializa
 		jobConfig2.put("awiscIndexingService", this);
 		jobConfig2.put("ldshService", ldshService);
 		
-		LdshIndexerJob nowJob = new LdshIndexerJob();
+	/*	LdshIndexerJob nowJob = new LdshIndexerJob();
 		nowJob.execute(new JobContext() {
 
 			@Override
@@ -185,7 +185,7 @@ public class AwiscIndexingServiceImpl implements AwiscIndexingService, Serializa
 			public Map<String, Serializable> getConfiguration() {
 				return jobConfig;
 			}
-		});
+		});*/
 		scheduler.schedule(new LdshIndexerJob(), scheduler.EXPR("0 */15 * * * ?").config(jobConfig2));
 	}
 
